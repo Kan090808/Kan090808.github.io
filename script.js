900c43d26e72adab4cc44948d1fc6c6a58ef82a4
kan <s104213059@mail1.ncnu.edu.tw>
2019-03-28 23:30:25 +0800

    feel good

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
   // 初始化
-  $("#main").load("intro.html");
-
+  $(".sidenav a").removeClass("active");
+  if (localStorage.getItem("page")) {
+    $("#" + localStorage.getItem("page")).addClass("active");
+    $("#main").load(localStorage.getItem("page") + ".html");
+  } else {
+    $("#intro").addClass("active");
+    $("#main").load("intro.html");
+  }
   $(".sidenav a").click(function () {
     var id = $(this).attr("id");
     var str = id + ".html";
     // alert(str);
+    localStorage.setItem("page", id);
     $(".sidenav a").removeClass("active");
     $(this).addClass("active");
     $("#main").load(str);
