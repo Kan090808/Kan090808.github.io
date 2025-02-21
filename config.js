c43eb3eee1df4499631cd12e96e46de9447db237
Jayden <kanjingterng@gmail.com>
2025-02-21 17:31:03 +0800

    test

---
+++ config.js
@@ -0,0 +1,11 @@
+const getFirebaseConfig = () => {
+    return {
+        apiKey: window.FIREBASE_API_KEY,
+        authDomain: window.FIREBASE_AUTH_DOMAIN,
+        databaseURL: window.FIREBASE_DATABASE_URL,
+        projectId: window.FIREBASE_PROJECT_ID,
+        storageBucket: window.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: window.FIREBASE_MESSAGING_SENDER_ID,
+        appId: window.FIREBASE_APP_ID
+    };
+};
